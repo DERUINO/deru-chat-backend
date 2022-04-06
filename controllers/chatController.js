1840b4edfa5da10c3ac42b0edebbde185ce728e0
Kirill Khromov <k.hromov@devspark.ru>
2022-04-07 00:21:42 +0300

    fix method getmessages

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -2,9 +2,9 @@ const Chat = require("../models/Chat");
 
 class chatController {
     async addMessage(req, res) {
-        const { text, authorId, recieveId, dialogId } = req.body
+        const { text, authorId, recieveId, dialogId, createdAt } = req.body
 
-        const message = new Chat({ text, authorId: authorId.id, recieveId, dialogId })
+        const message = new Chat({ text, authorId: authorId._id, recieveId, dialogId, createdAt, updatedAt: createdAt })
         await message.save()
         res.json({status: 200, data: message})
     }
@@ -21,6 +21,8 @@ class chatController {
                     ]
                 }
             )
+            .skip(await Chat.count() - 10)
+            .sort({ createdAt: 1 })
             .populate(['authorId', 'recieveId'])
             .lean()
         res.json({ status: 200, data: messages })
--- models/Chat.js
+++ models/Chat.js
@@ -7,8 +7,8 @@ const Chat = new Schema({
     text: { type: String, required: true },
     authorId: { type: ObjectId, required: true, ref: 'Account' },
     recieveId: { type: ObjectId, required: true, ref: 'Account' },
-    createdAt: { type: Date, required: false, default: Date.now() },
-    updatedAt: { type: Date, required: false, default: Date.now() },
+    createdAt: { type: Date, required: false },
+    updatedAt: { type: Date, required: false },
 })
 
 module.exports = model('Chat', Chat)
